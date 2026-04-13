b4948a29a9621b8240c1d240a6417698fb9c84d9
Abhinav Walunj <abhinavwalunj2001@gmail.com>
2026-04-13 10:14:37 +0530

    Integrated Webflux and netty resolver into our project for external api call.

---
+++ src/main/java/com/stockanalyzer/ingestion/client/StockApiClient.java
@@ -0,0 +1,29 @@
+package com.stockanalyzer.ingestion.client;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class StockApiClient {
+
+    private final WebClient webClient;
+
+    public StockApiClient() {
+        this.webClient = WebClient.builder()
+                .baseUrl("https://www.alphavantage.co")
+                .build();
+    }
+
+    public String fetchStock(String symbol) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/query")
+                        .queryParam("function", "GLOBAL_QUOTE")
+                        .queryParam("symbol", symbol)
+                        .queryParam("apikey", "YOUR_API_KEY")
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+}
